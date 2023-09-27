82d592ab837332026c0657d980670631a3b89d1a
Alex Locke <alexhlocke@gmail.com>
2023-09-27 00:55:34 -0700

    pre-test stuff

--- Assets/Scripts/PlacementMenu.cs
+++ Assets/Scripts/PlacementMenu.cs
@@ -4,5 +4,46 @@ using UnityEngine;
 
 public class PlacementMenu : MonoBehaviour
 {
-    
+    [Header("References")]
+    public GameObject StateManagerObject;
+    public GameObject MainCanvas;
+    public GameObject PlacementCanvas;
+
+    private StateManager stateManager;
+
+
+    public void Awake() {
+        stateManager = StateManagerObject.GetComponent<StateManager>();
+    }
+
+    public void StartFight() {
+        // Find all GameObjects with the 'enemy' or 'troop' tag.
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy");
+        GameObject[] troops = GameObject.FindGameObjectsWithTag("troop");
+
+        // Combine the two arrays.
+        GameObject[] allUnits = new GameObject[enemies.Length + troops.Length];
+        enemies.CopyTo(allUnits, 0);
+        troops.CopyTo(allUnits, enemies.Length);
+
+        // Loop through each GameObject and set Movement component to active.
+        foreach (GameObject unit in allUnits) {
+            Movement movementComponent = unit.GetComponent<Movement>();
+            // Catch oopsie woopsie fucky wuckies
+            if (movementComponent != null) {
+                movementComponent.enabled = true;
+                movementComponent.FindEnemies(); // Reset enemy list
+            }
+            else {
+                Debug.LogWarning("No 'Movement' component found on " + unit.name);
+            }
+        }
+
+        // Change game state
+        stateManager.SetGameState(StateManager.GameState.Playing);
+
+        // Set canvases to active/inactive
+        MainCanvas.SetActive(true);
+        PlacementCanvas.SetActive(false);
+    }
 }
